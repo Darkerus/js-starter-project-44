f8d3ba7e0a3c5f18a06bb0335aa03a93eded8a2d
Darkon <9017059@gmail.com>
2023-07-21 21:46:20 +0300

    3rd step complete

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readLineSync from 'readline-sync';
+
+export default function () {
+	console.log('May I have your name?');
+	const name = readLineSync.question('Your answer: ');
+	console.log('Hello, ' + name + '!');
+}
